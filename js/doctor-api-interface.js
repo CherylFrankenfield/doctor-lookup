b9628a6730c7354e295d312afd722d2413c86116
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-12 16:04:46 -0800

    work on selecting nested arrays in search object and investigate for-in loop

--- js/doctor-api-interface.js
+++ js/doctor-api-interface.js
@@ -11,14 +11,17 @@ $(document).ready(function() {
 
     doctor.then(function(response){
       const search = JSON.parse(response);
-      console.log(search.data[0].profile[0]);
-      if (response) {
-        $('#results').append(`<li>${search.data.profile.first_name}</li></br>`);
-        $('#attribution').append(`<p>Powered by <a href="https://betterdoctor.com">BetterDoctor.</a></p>`);
+      for (var i=0; i > search.length, i++) {
+        document.getElementById("results").innerHTML = search.data.profile.first_name + ", " + search.data.profile.last_name;
       }
+      // console.log(search);
+      // console.log(search.data.profile[0]);
+      // if (search.data.profile[0]) {
+      //   $('#results').append(`<li>${search.data.profile[0]}</li></br>`);
+      //   $('#attribution').append(`<p>Powered by <a href="https://betterdoctor.com">BetterDoctor.</a></p>`);
+      // }
     }, function(){
       alert("We apologize for the inconvenience. Our servers are overloaded, please try again momentarily.")
     });
   });
 });
-// response.abilities[0].ability.name
