8a4b7a6701fb39eb464badc8101d55bb2693e69e
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-15 11:37:08 -0800

    work on firstname list

--- js/doctor-api-interface.js
+++ js/doctor-api-interface.js
@@ -13,12 +13,21 @@ $(document).ready(function() {
       const search = JSON.parse(response);
       console.log(search);
       // console.log(search.meta.ignored_query_parameters);
-      console.log(search.data[0].profile.first_name[0]);
-      console.log(search.data.profile.last_name[0]);
-      if (search.meta.ignored_query_parameters) {
-        $('#results').append(`<li>${search.meta.ignored_query_parameters}</li></br>`);
-        $('#attribution').append(`<p>Powered by <a href="https://betterdoctor.com">BetterDoctor.</a></p>`);
+      console.log(search.data[1].profile.first_name);
+      const results = [];
+      for (let i = 0; i < search.length; i++) {
+        search.data[i].profile.map(function(profile){
+          if (profile.first_name) {
+            $('#news-output').append(`<li>${profile.first_name}</li></br>`);
+          }
+        });
       }
+      console.log(profile.first_name);
+      // console.log(search.data.profile.last_name[0]);
+      // if (search.meta.ignored_query_parameters) {
+      //   $('#results').append(`<li>${search.meta.ignored_query_parameters}</li></br>`);
+      //   $('#attribution').append(`<p>Powered by <a href="https://betterdoctor.com">BetterDoctor.</a></p>`);
+      // }
     }, function(){
       alert("We apologize for the inconvenience. Our servers are overloaded, please try again momentarily.")
     });
