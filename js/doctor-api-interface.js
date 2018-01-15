f93d2bb62788f850b144cef2037daff4bac1a2d0
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-15 13:09:17 -0800

    App displays first name of doctor

--- js/doctor-api-interface.js
+++ js/doctor-api-interface.js
@@ -12,25 +12,17 @@ $(document).ready(function() {
     doctor.then(function(response){
       const search = JSON.parse(response);
       console.log(search);
-      // console.log(search.meta.ignored_query_parameters);
       console.log(search.data[1].profile.first_name);
-      console.log(search.data[2].profile.first_name);
-      console.log(search.data[3].profile.first_name);
       console.log(search.data[1].profile.last_name);
-      console.log(search.data[2].profile.last_name);
-      console.log(search.data[3].profile.last_name);
       console.log(search.data[4].specialties[0]);
       console.log(search.data[6].practices[0].accepts_new_patients);
       console.log(search.data[6].practices[0].visit_address.street);
       console.log(search.data[6].practices[0].visit_address.zip);
       console.log(search.data[6].practices[2].phones[0].number);
-      const results = [];
-      for (let i = 0; i < search.length; i++) {
-        search.data[i].profile.map(function(profile){
-          if (profile.first_name) {
-            $('#news-output').append(`<li>${profile.first_name}</li></br>`);
-          }
-        });
+      for (let i = 0; i < search.data[i].profile.last_name.length; i++) {
+          if (search.data[i].profile.first_name) {
+            $('#results').append(`<li>${search.data[i].profile.first_name}</li></br>`);
+          } 
       }
       // console.log(search.data.profile.last_name[0]);
       // if (search.meta.ignored_query_parameters) {
