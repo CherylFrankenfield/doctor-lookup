b75e1701f4618379291069883a4a8bddda55c674
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-18 20:44:25 -0800

    Successfully getting street address info to append in FE

--- js/doctor-api-interface.js
+++ js/doctor-api-interface.js
@@ -14,11 +14,21 @@ $(document).ready(function() {
       console.log(allResults);
       // console.log(typeof allResults);
       for (let i = 0; i < allResults.length; i++) {
-        $('#results').append(`<li>First Name: ${allResults[i]}</li></br>`
-        `<li>Last Name: ${allResults[i]}</li></br>`
-        `<li> Street Address: ${allResults[i]}</li></br>`
-        `<li>Portland,OR, ${allResults[i]}</li></br>`
-        `<li>${allResults[i]}</li></br>`);
+        if (allResults[i][0] === first) {
+          $('#results').append(`<li>First Name: ${first}</li></br>`);
+        } else if (allResults[i][0] === last) {
+          $('#results').append(`<li>Last Name: ${last}</li></br>`);
+        } else if (allResults[i][0]) {
+          $('#results').append(`<li> Street Address: ${allResults[i][2]} Portland, OR, </li></br>`);
+        } else if (allReults[i][0]) {
+          $('#results').append(`<li> Zip: ${allResults[i][3]}</li></br>`);
+        } else if (allReults[i][0]) {
+          $('#results').append(`<li>Phone: ${allResults[i][4]}</li></br>`);
+        }
+        // `<li>Last Name: ${allResults[i]}</li></br>`
+        // `<li> Street Address: ${allResults[i]}</li></br>`
+        // `<li>Portland,OR, ${allResults[i]}</li></br>`
+        // `<li>${allResults[i]}</li></br>`);
       }
       $('#attribution').append(`<p>Powered by <a href="https://betterdoctor.com">BetterDoctor.</a></p>`);
 
--- js/doctor-api-scripts.js
+++ js/doctor-api-scripts.js
@@ -9,7 +9,7 @@
 //   }
 // }
 
-export function doctorApi(first,last,address,zip,phone,ailment) {
+export function doctorApi(first,last,address,zip,phone) {
   return new Promise(function(success, error) {
     const request = new XMLHttpRequest();
     const url = `https://api.betterdoctor.com/2016-03-01/doctors/?location=45.523,-122.676,100&skip=2&limit=10&user_key=e44a69d0bf47478c768f57e63e159366&data.profile.first_name=${first}&data.profile.last_name=${last}&data.practices.phones.number=${phone}&data.practices.visit_address.street=${address}&data.practices.visit_address.zip=${zip}`;
@@ -26,7 +26,7 @@ export function doctorApi(first,last,address,zip,phone,ailment) {
           const address = search.data[i].practices[0].visit_address.street;
           const zip = search.data[i].practices[0].visit_address.zip;
           const phone = search.data[i].practices[0].phones[0].number;
-          const results = [first, last, address, zip, phone, ailment];
+          const results = [first, last, address, zip, phone];
           allResults.push(results);
 
           success(allResults);
