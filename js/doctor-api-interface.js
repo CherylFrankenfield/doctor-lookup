894b70ff0efb652256d41b924496940af762463f
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-01-18 21:10:33 -0800

    Front End outputs all doctors with 5 data points to front end in list, search url limit is 10

--- js/doctor-api-interface.js
+++ js/doctor-api-interface.js
@@ -14,17 +14,8 @@ $(document).ready(function() {
       console.log(allResults);
       // console.log(typeof allResults);
       for (let i = 0; i < allResults.length; i++) {
-        if (allResults[i][0] === first) {
-          $('#results').append(`<li>First Name: ${first}</li></br>`);
-        } else if (allResults[i][0] === last) {
-          $('#results').append(`<li>Last Name: ${last}</li></br>`);
-        } else if (allResults[i][0]) {
-          $('#results').append(`<li> Street Address: ${allResults[i][2]} Portland, OR, </li></br>`);
-        } else if (allReults[i][0]) {
-          $('#results').append(`<li> Zip: ${allResults[i][3]}</li></br>`);
-        } else if (allReults[i][0]) {
-          $('#results').append(`<li>Phone: ${allResults[i][4]}</li></br>`);
-        }
+        $('#results').append(`<li>Doctor: ${allResults[i]}</li></br>`);
+
         // `<li>Last Name: ${allResults[i]}</li></br>`
         // `<li> Street Address: ${allResults[i]}</li></br>`
         // `<li>Portland,OR, ${allResults[i]}</li></br>`
